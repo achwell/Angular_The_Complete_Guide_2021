aecf08f2bf215890d844a96f50973f995a82c59c
Axel Wulff Sæther <axelwulff@mac.com>
2021-08-21 09:39:07 +0200

    Section 2: The Basics

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -1,23 +1,47 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
-  template: `
-    <h3>Servers</h3>
-    <app-server></app-server>
-    <app-server></app-server>
-  `,
+  selector: '[app-servers]',
+  templateUrl: './servers.component.html',
   styles: [`
-    h3 {
+    label {
       color: #1e90ff;
+    }
+    .white-text {
+      color: #ffffff;
     }
   `]
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created';
+  serverName = 'testServer';
+  serverCreated = false;
+  username = '';
+  servers = ['TestServer1', 'TestServer2'];
+  showText: boolean = false;
+  log = [];
 
-  constructor() { }
+  constructor() {
+    setTimeout(() => this.allowNewServer = true, 2000);
+  }
 
   ngOnInit(): void {
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = `${this.serverName} was created`;
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
+  }
+
+  onUpdateServerName(event: any) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
+
+  toggleText() {
+    this.showText = !this.showText;
+    this.log.push(new Date());
   }
 
 }
