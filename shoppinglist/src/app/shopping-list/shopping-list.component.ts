24e7ee95d81be5866c213eefb98d55f2cc82ddab
Axel Wulff Sæther <axelwulff@mac.com>
2021-08-24 16:37:17 +0200

    Section 6: Course Project - Components & Databinding

--- shoppinglist/src/app/shopping-list/shopping-list.component.ts
+++ shoppinglist/src/app/shopping-list/shopping-list.component.ts
@@ -7,10 +7,7 @@ import {Ingredient} from "../shared/ingredient.model";
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-  ingredients: Ingredient[] = [
-    new Ingredient("Egg", 5),
-    new Ingredient("Tomater", 10)
-  ];
+  ingredients: Ingredient[] = [];
 
   constructor() { }
 
