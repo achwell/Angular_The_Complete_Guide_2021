0c26b95d39e04088b3403184f78002f7eadf680a
Axel Wulff Sæther <axelwulff@mac.com>
2021-08-26 18:11:53 +0200

    Section 8: Course Project - Directives

--- recipes/src/app/app.module.ts
+++ recipes/src/app/app.module.ts
@@ -10,10 +10,12 @@ import {RecipeDetailComponent} from './recipes/recipe-detail/recipe-detail.compo
 import {RecipeItemComponent} from './recipes/recipe-list/recipe-item/recipe-item.component';
 import {ShoppingListComponent} from './shopping-list/shopping-list.component';
 import {ShoppingEditComponent} from './shopping-list/shopping-edit/shopping-edit.component';
+import {DropdownDirective} from "./shared/dropdown-directive";
 
 @NgModule({
   declarations: [
     AppComponent,
+    DropdownDirective,
     HeaderComponent,
     RecipesComponent,
     RecipeListComponent,
